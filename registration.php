dc1c3ab28f460cf28a4eee2ac65901c0596a5c75
Iman Aboheydary <iranimij@gmail.com>
2024-11-28 02:17:35 +0100

    Update example project

--- registration.php
+++ registration.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'Iranimij_GitExtensionChecker',
